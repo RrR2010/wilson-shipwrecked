3f23e99f7b792b3afcd626d8b6d7ce20135c073d
RrR2010 <79206858+RrR2010@users.noreply.github.com>
2026-08-31 17:13:53 -0300

    refactor: type observed event identity

--- src/domain/cognition/observed_event.gd
+++ src/domain/cognition/observed_event.gd
@@ -1,10 +1,12 @@
 class_name ObservedEvent
 extends RefCounted
 
+const DomainId = preload("res://src/domain/core/domain_id.gd")
+
 ## Wilson-accessible projection of an authoritative WorldEvent.
 ## This object is not World truth and may omit roles/details the world knows.
 
-var event_type: StringName
+var event_type
 var action_id
 var execution_id: StringName
 var perceived_bindings: Dictionary
@@ -12,13 +14,15 @@ var modalities: Array[StringName]
 
 
 func _init(
-	p_event_type: StringName,
+	p_event_type,
 	p_action_id,
 	p_execution_id: StringName,
 	p_perceived_bindings: Dictionary,
 	p_modalities: Array[StringName]
 ) -> void:
-	assert(p_event_type != &"", "ObservedEvent requires event_type")
+	assert(p_event_type != null, "ObservedEvent requires event_type")
+	assert(p_event_type is Object and p_event_type.has_method("assert_kind"), "ObservedEvent event_type must be EventDefinitionId")
+	p_event_type.assert_kind(DomainId.Kind.EVENT_DEFINITION)
 	assert(p_action_id != null, "ObservedEvent requires action_id")
 	assert(p_execution_id != &"", "ObservedEvent requires execution_id")
 	event_type = p_event_type
