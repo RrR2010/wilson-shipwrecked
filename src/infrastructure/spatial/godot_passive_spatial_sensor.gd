d3a1a8b732845b95abe7ed0b87f947be86ca83a0
RrR2010 <79206858+RrR2010@users.noreply.github.com>
2026-09-03 22:09:06 -0300

    Instrument passive spatial overlap reconciliation

--- src/infrastructure/spatial/godot_passive_spatial_sensor.gd
+++ src/infrastructure/spatial/godot_passive_spatial_sensor.gd
@@ -14,6 +14,7 @@ extends Node
 const MOVING_RECONCILE_FRAMES := 6
 const STATIC_RECONCILE_FRAMES := 60
 const MOVEMENT_REFRESH_DISTANCE := 0.10
+const DEBUG_BUILD := "passive_sensor_reconcile_v2"
 
 var _sensor_area: Area3D
 var _ref_by_collision_id: Dictionary = {}
@@ -22,6 +23,7 @@ var _dirty := false
 var _physics_frames_since_reconcile: int = 0
 var _last_reconcile_position := Vector3.ZERO
 var _has_reconcile_position := false
+var _reconcile_count: int = 0
 
 
 func configure(sensor_area: Area3D) -> void:
@@ -35,7 +37,9 @@ func configure(sensor_area: Area3D) -> void:
 	_sensor_area.area_exited.connect(_on_area_exited)
 	_physics_frames_since_reconcile = 0
 	_has_reconcile_position = false
+	_reconcile_count = 0
 	set_physics_process(true)
+	print("[PASSIVE_SENSOR][BUILD] %s mask=%d monitoring=%s" % [DEBUG_BUILD, _sensor_area.collision_mask, _sensor_area.monitoring])
 
 
 func bind_candidate(runtime_ref, collision_object: CollisionObject3D) -> bool:
@@ -45,6 +49,7 @@ func bind_candidate(runtime_ref, collision_object: CollisionObject3D) -> bool:
 	if _ref_by_collision_id.has(instance_id):
 		return _ref_by_collision_id[instance_id].equals(runtime_ref)
 	_ref_by_collision_id[instance_id] = runtime_ref
+	print("[PASSIVE_SENSOR][BIND] instance=%d layer=%d ref=%s" % [instance_id, collision_object.collision_layer, runtime_ref.sort_key()])
 	return true
 
 
@@ -81,10 +86,12 @@ func active_candidate_count() -> int:
 func reconcile_overlaps() -> bool:
 	if _sensor_area == null or not is_instance_valid(_sensor_area) or not _sensor_area.is_inside_tree():
 		return false
+	var overlapping_bodies: Array[Node3D] = _sensor_area.get_overlapping_bodies()
+	var overlapping_areas: Array[Area3D] = _sensor_area.get_overlapping_areas()
 	var current_by_key: Dictionary = {}
-	for body in _sensor_area.get_overlapping_bodies():
+	for body in overlapping_bodies:
 		_collect_bound_overlap(body, current_by_key)
-	for area in _sensor_area.get_overlapping_areas():
+	for area in overlapping_areas:
 		_collect_bound_overlap(area, current_by_key)
 
 	var changed: bool = not _same_key_set(_active_by_key, current_by_key)
@@ -94,6 +101,16 @@ func reconcile_overlaps() -> bool:
 	_last_reconcile_position = _sensor_area.global_position
 	_has_reconcile_position = true
 	_physics_frames_since_reconcile = 0
+	_reconcile_count += 1
+	if changed or not overlapping_bodies.is_empty() or not overlapping_areas.is_empty() or _reconcile_count == 1:
+		print("[PASSIVE_SENSOR][RECONCILE] count=%d raw_bodies=%d raw_areas=%d bound=%d changed=%s position=%s" % [
+			_reconcile_count,
+			overlapping_bodies.size(),
+			overlapping_areas.size(),
+			current_by_key.size(),
+			changed,
+			str(_sensor_area.global_position),
+		])
 	return changed
 
 
@@ -119,18 +136,22 @@ func _physics_process(_delta: float) -> void:
 
 
 func _on_body_entered(body: Node3D) -> void:
+	print("[PASSIVE_SENSOR][SIGNAL] body_entered id=%d" % body.get_instance_id())
 	_enter_collision_object(body)
 
 
 func _on_body_exited(body: Node3D) -> void:
+	print("[PASSIVE_SENSOR][SIGNAL] body_exited id=%d" % body.get_instance_id())
 	_exit_collision_object(body)
 
 
 func _on_area_entered(area: Area3D) -> void:
+	print("[PASSIVE_SENSOR][SIGNAL] area_entered id=%d" % area.get_instance_id())
 	_enter_collision_object(area)
 
 
 func _on_area_exited(area: Area3D) -> void:
+	print("[PASSIVE_SENSOR][SIGNAL] area_exited id=%d" % area.get_instance_id())
 	_exit_collision_object(area)
 
 
@@ -139,11 +160,13 @@ func _enter_collision_object(node: Object) -> void:
 		return
 	var runtime_ref = _ref_by_collision_id.get(node.get_instance_id())
 	if runtime_ref == null:
+		print("[PASSIVE_SENSOR][UNBOUND] entered id=%d" % node.get_instance_id())
 		return
 	var key = runtime_ref.key()
 	if not _active_by_key.has(key):
 		_active_by_key[key] = runtime_ref
 		_dirty = true
+		print("[PASSIVE_SENSOR][CANDIDATE] entered=%s" % runtime_ref.sort_key())
 
 
 func _exit_collision_object(node: Object) -> void:
@@ -154,6 +177,7 @@ func _exit_collision_object(node: Object) -> void:
 		return
 	if _active_by_key.erase(runtime_ref.key()):
 		_dirty = true
+		print("[PASSIVE_SENSOR][CANDIDATE] exited=%s" % runtime_ref.sort_key())
 
 
 func _collect_bound_overlap(node: Object, output: Dictionary) -> void:
