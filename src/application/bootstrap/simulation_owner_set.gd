c41a48758d8feea8374b7f70666086ef437e9272
RrR2010 <79206858+RrR2010@users.noreply.github.com>
2026-09-05 18:40:46 -0300

    Expose DriveState from shared bootstrap owner set

--- src/application/bootstrap/simulation_owner_set.gd
+++ src/application/bootstrap/simulation_owner_set.gd
@@ -10,6 +10,7 @@ var wilson_world_state
 var beliefs
 var current_intention
 var wilson_body_state
+var drives
 
 func _init(
 	p_entities,
@@ -17,7 +18,8 @@ func _init(
 	p_wilson_world_state,
 	p_beliefs,
 	p_current_intention,
-	p_wilson_body_state
+	p_wilson_body_state,
+	p_drives
 ) -> void:
 	entities = p_entities
 	relations = p_relations
@@ -25,3 +27,4 @@ func _init(
 	beliefs = p_beliefs
 	current_intention = p_current_intention
 	wilson_body_state = p_wilson_body_state
+	drives = p_drives
