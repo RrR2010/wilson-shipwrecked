760fb31f96c2df8e50f1ef4f76419042106e4109
RrR2010 <79206858+RrR2010@users.noreply.github.com>
2026-09-05 02:06:56 -0300

    Record fake motion requests and cancellations

--- tests/fakes/fake_motion_port.gd
+++ tests/fakes/fake_motion_port.gd
@@ -3,14 +3,18 @@ extends "res://src/application/simulation/motion_port.gd"
 
 var _status_by_actor: Dictionary = {}
 var _target_by_actor: Dictionary = {}
+var request_history: Array = []
+var cancel_history: Array = []
 
 func request_move(actor_ref: RuntimeWorldRef, target_ref: RuntimeWorldRef) -> bool:
 	_target_by_actor[actor_ref.key()] = target_ref
 	_status_by_actor[actor_ref.key()] = MotionStatus.MOVING
+	request_history.append({"actor": actor_ref, "target": target_ref})
 	return true
 
 func cancel_move(actor_ref: RuntimeWorldRef) -> void:
 	var key: StringName = actor_ref.key()
+	cancel_history.append(actor_ref)
 	if _status_by_actor.has(key):
 		_status_by_actor[key] = MotionStatus.CANCELLED
 
